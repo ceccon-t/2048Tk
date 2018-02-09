d15a27bbe63dd8cae3ad70cdf74bf48c94a75761
Tiago Ceccon <ceccon.t@hotmail.com>
2018-02-09 00:12:51 -0200

    Adds Help menu

--- 2048Tk.py
+++ 2048Tk.py
@@ -16,6 +16,10 @@ def keydown(event):
         Processes user input
     """
     global board, playing, statusMessage
+
+    if event.keysym == "F1":
+        help_gameplay()
+
     if playing:
         if event.char == 'w' or event.keysym == "Up":
             board.move(UP)
@@ -48,10 +52,60 @@ def new_game():
     playing = True
 
 
+# Helper functions for menus
+def help_gameplay():
+    """
+        Shows window with information on how to play
+    """
+    msg_gameplay = "W or Up arrow: move tiles up\n" \
+                   "S or Down arrow: move tiles down\n" \
+                   "A or Left arrow: move tiles to the left\n" \
+                   "D or Right arrow: move tiles to the right\n" \
+                   "\nBoard starts with two tiles with values 2 or 4, you can\n" \
+                   "slide tiles up, down, left or right with the keys listed above.\n" \
+                   "Each time two tiles with the same value 'slide next to one another'\n" \
+                   "they merge, with the final tile receiving the sum of both tiles.\n" \
+                   "If no tile can move in the specified direction, move is ignored.\n" \
+                   "If a move is processed, an empty tile receives a value (either 2 or 4).\n" \
+                   "Your goal is to produce a tile with the value 2048.\n" \
+                   "Game is over when either the goal is achieved or no more moves are possible."
+    help_gameplay_window = Toplevel(root, padx=10, pady=10)
+    help_gameplay_window.title("How to")
+    help_gameplay_window.transient(root)
+    help_gameplay_window.resizable(False, False)
+    Label(help_gameplay_window, text=msg_gameplay, padx=25, pady=25).pack()
+    Button(help_gameplay_window, text="OK", command=help_gameplay_window.destroy).pack()
+
+
+def help_about():
+    """
+        Shows window with information about the program
+    """
+    msg_about = "Tkinter clone of the game 2048, made to explore this library's ins and outs."
+    help_about_window = Toplevel(root, padx=10, pady=10)
+    help_about_window.title("About")
+    help_about_window.transient(root)
+    help_about_window.resizable(False, False)
+    Label(help_about_window, text=msg_about, padx=25, pady=25).pack()
+    Button(help_about_window, text="Ok", command=help_about_window.destroy).pack()
+
+
 # SETTING THINGS UP
 
 # Main window
 root = Tk()
+
+# Menus
+menu_bar = Menu(root)
+
+# Help Menu
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="How to play", command=help_gameplay, accelerator='F1', underline=0)
+help_menu.add_command(label="About", command=help_about, underline=0)
+
+# End of Menus
+root.config(menu=menu_bar)
 
 # Base frame for everything
 topFrame = Frame(root, padx=10, pady=10)
@@ -90,7 +144,6 @@ playing = True
 
 
 # START GAME
-
 root.geometry("+400+75")
 root.title("2048Tk")
 root.mainloop()
